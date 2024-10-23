9cdd214c31f4138675f3db2fac88fb8944df331e
Tavi Castro <hanminhdanh1325@gmail.com>
2024-10-23 16:06:31 +0700

    fix: fixed

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { AppRoute } from '@/routes'
 
 const App: React.FC = () => {
+  console.log('fixed')
   return (
     <BrowserRouter>
       <AppRoute />
